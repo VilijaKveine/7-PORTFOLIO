414049da2065239adb5a77e2611de0a274b6fd00
VilijaKveine <vilija.cizaite@gmail.com>
2020-01-08 20:14:01 -0800

    achievements/progress-bar

--- js/actions.js
+++ js/actions.js
@@ -1,1 +1,7 @@
 "use strict";
+
+renderAchievements( achievements );
+
+renderSkills( skills );
+
+renderGallery( '#latest_works_gallery', works );
--- js/data.js
+++ js/data.js
@@ -1,1 +1,54 @@
 "use strict";
+
+const achievements = [
+    {
+        icon: 'archive',
+        number: 245,
+        title: 'Total projects'
+    },
+    'easdsfdj',
+    4665565,
+    true,
+    false,
+    null,
+    [],
+    {
+        icon: '',
+        number: 0,
+        title: 'Trophy won'
+    },
+    {
+        icon: 'trophy',
+        number: 2535,
+        title: 'Trophy won'
+    },
+    {
+        icon: 'users',
+        number: 288,
+        title: 'Experiences staff'
+    },
+    {
+        icon: 'smile-o',
+        number: 750,
+        title: 'Happy clients'
+    }
+]
+    
+const skills = [
+    {
+        title: 'HTML5',
+        value: 85
+    },
+    {
+        title:'CSS3',
+        value:90
+    },
+    {
+        title: 'J-Query',
+        value: 90
+    },
+    {
+        title: 'Bootstrap',
+        value: 60
+    }
+]
